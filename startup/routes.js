690b7b9328d30335d3dbad0598b615529b020c91
nascal3 <nascal3@gmail.com>
2019-01-30 13:31:10 +0300

    created model for vendor ledger table

--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const suppUsers = require('../routes/suppUsers');
+const vendorLedg = require('../routes/vendorLedg');
 
 module.exports = function (app) {
   app.use(express.json());
   app.use('/api/suppliers', suppUsers);
+  app.use('/api/vendor', vendorLedg);
 };
