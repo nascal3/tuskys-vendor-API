0b89604b49703ee1a4b72a9b9a551bbc7ac08d0c
nascal3 <nascal3@gmail.com>
2019-02-15 12:57:57 +0300

    removed querying if vendor items ledger

--- routes/suppUsers.js
+++ routes/suppUsers.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const SuppUsers = require('../models/SuppUsersModel');
-const VendorLedgEntry = require('../models/VendorLedgEntryModel');
 require('express-async-errors');
 
 /* GET users listing. */
@@ -17,27 +16,6 @@ router.get('/:page', async (req, res) => {
   offset = limit * (page - 1);
 
   const users = await SuppUsers.findAll({
-    attributes: {exclude: ['timestamp']},
-    limit: limit,
-    offset: offset
-  });
-
-  res.status(200).json({'result': users, 'count': data.count, 'pages': pages});
-});
-
-router.get('/ledger/:page', async (req, res) => {
-
-  let limit = 50;   // number of records per page
-  let offset;
-
-  const data = await VendorLedgEntry.findAndCountAll();
-  let page = parseInt(req.params.page);      // page number
-  page <= 0 ? page = 1 : page = parseInt(req.params.page);
-  let pages = Math.ceil(data.count / limit);
-  offset = limit * (page - 1);
-  console.log('>>>', pages);
-
-  const users = await VendorLedgEntry.findAll({
     attributes: {exclude: ['timestamp']},
     limit: limit,
     offset: offset
