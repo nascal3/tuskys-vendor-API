a56b8dc1dac5c2a44d2cf8fa260b1c11454a9fc4
nascal3 <nascal3@gmail.com>
2019-02-16 10:30:27 +0300

    fetch an item and all its relevant transactions

--- routes/items.js
+++ routes/items.js
@@ -28,4 +28,37 @@ router.get('/:page', async (req, res) => {
   res.status(200).json({'result': items});
 });
 
+// GET AN ITEM WITH THE ITEM NUMBER
+router.get('/item/:itemNum/:page', async (req, res) => {
+
+  let limit = 10;   // number of records per page
+  let offset;
+
+  let page = parseInt(req.params.page);      // page number
+  page <= 0 ? page = 1 : page = parseInt(req.params.page);
+  offset = limit * (page - 1);
+
+  const item = await ItemModel.findAll({
+    order: [
+      [{model:TransSalesEntryModel, as: 'Sales'}, 'Transaction_No', 'DESC']
+    ],
+
+    include: [{
+      model:TransSalesEntryModel,
+      as: 'Sales',
+      attributes: {exclude: ['timestamp']}
+    }],
+    where: { No: req.params.itemNum },
+    attributes: {exclude: ['timestamp']}
+  });
+
+  //Filter out only all transactions and store in array
+  const sales = [];
+  item.map(trans => {
+    sales.push(trans.Sales);
+  });
+
+  res.status(200).json({'result': item[0], 'transactions': sales});
+});
+
 module.exports = router;
