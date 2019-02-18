5d5577c0d83eb29c24cc91f788e742423b9e03bb
nascal3 <nascal3@gmail.com>
2019-02-18 10:49:53 +0300

    removed unwanted columns in trans sales entry model

--- models/TransSalesEntryModel.js
+++ models/TransSalesEntryModel.js
@@ -46,11 +46,6 @@ const TransSalesEntryModel = sequelize.define('TransSalesEntry', {
     field: 'Item No_',
     allowNull: false
   },
-  Sales_Staff: {
-    type: Sequelize.STRING(20),
-    field: 'Sales Staff',
-    allowNull: false
-  },
   Item_Category_Code: {
     type: Sequelize.STRING(10),
     field: 'Item Category Code',
@@ -74,11 +69,6 @@ const TransSalesEntryModel = sequelize.define('TransSalesEntry', {
     type: Sequelize.DECIMAL,
     allowNull: false
   },
-  Price_Group_Code: {
-    type: Sequelize.STRING(10),
-    field: 'Price Group Code',
-    allowNull: false
-  },
   VAT_Bus_Posting_Group: {
     type: Sequelize.STRING(10),
     field: 'VAT Bus_ Posting Group',
@@ -87,16 +77,6 @@ const TransSalesEntryModel = sequelize.define('TransSalesEntry', {
   VAT_Code: {
     type: Sequelize.STRING(10),
     field: 'VAT Code',
-    allowNull: false
-  },
-  xTransaction_Status: {
-    type: Sequelize.INTEGER,
-    field: 'xTransaction Status',
-    allowNull: false
-  },
-  Discount_Amount: {
-    type: Sequelize.DECIMAL,
-    field: 'Discount Amount',
     allowNull: false
   },
   Cost_Amount: {
@@ -110,11 +90,6 @@ const TransSalesEntryModel = sequelize.define('TransSalesEntry', {
   },
   Time: {
     type: Sequelize.DATE,
-    allowNull: false
-  },
-  Shift_No: {
-    type: Sequelize.STRING(1),
-    field: 'Shift No_',
     allowNull: false
   },
   Shift_Date: {
@@ -147,92 +122,9 @@ const TransSalesEntryModel = sequelize.define('TransSalesEntry', {
     field: 'Disc_ Amount From Std_ Price',
     allowNull: false
   },
-  xStatement_No: {
-    type: Sequelize.STRING(20),
-    field: 'xStatement No_',
-    allowNull: false
-  },
   Customer_No: {
     type: Sequelize.STRING(20),
     field: 'Customer No_',
-    allowNull: false
-  },
-  Section: {
-    type: Sequelize.STRING(10),
-    allowNull: false
-  },
-  Shelf: {
-    type: Sequelize.STRING(10),
-    allowNull: false
-  },
-  Statement_Code: {
-    type: Sequelize.STRING(20),
-    field: 'Statement Code',
-    allowNull: false
-  },
-  Item_Disc_Group: {
-    type: Sequelize.STRING(10),
-    field: 'Item Disc_ Group',
-    allowNull: false
-  },
-  Transaction_Code: {
-    type: Sequelize.INTEGER,
-    field: 'Transaction Code',
-    allowNull: false
-  },
-  Item_Number_Scanned: {
-    type: Sequelize.INTEGER,
-    field: 'Item Number Scanned',
-    allowNull: false
-  },
-  Keyboard_Item_Entry: {
-    type: Sequelize.INTEGER,
-    field: 'Keyboard Item Entry',
-    allowNull: false
-  },
-  Price_in_Barcode: {
-    type: Sequelize.INTEGER,
-    field: 'Price in Barcode',
-    allowNull: false
-  },
-  Price_Change: {
-    type: Sequelize.INTEGER,
-    field: 'Price Change',
-    allowNull: false
-  },
-  Weight_Manually_Entered: {
-    type: Sequelize.INTEGER,
-    field: 'Weight Manually Entered',
-    allowNull: false
-  },
-  Line_was_Discounted: {
-    type: Sequelize.INTEGER,
-    field: 'Line was Discounted',
-    allowNull: false
-  },
-  Weight_Item: {
-    type: Sequelize.INTEGER,
-    field: 'Weight Item',
-    allowNull: false
-  },
-  Return_No_Sale: {
-    type: Sequelize.INTEGER,
-    field: 'Return No Sale',
-    allowNull: false
-  },
-  Item_Corrected_Line: {
-    type: Sequelize.INTEGER,
-    field: 'Item Corrected Line',
-    allowNull: false
-  },
-  Orig_of_Linked_Item_List: {
-    type: Sequelize.INTEGER,
-    field: 'Orig_ of a Linked Item List',
-    allowNull: false
-  },
-  Linked_No_not_Orig: {
-    type: Sequelize.INTEGER,
-    field: 'Linked No_ not Orig_',
     allowNull: false
   },
   Staff_ID: {
@@ -245,167 +137,14 @@ const TransSalesEntryModel = sequelize.define('TransSalesEntry', {
     field: 'Item Posting Group',
     allowNull: false
   },
-  Counter: {
-    type: Sequelize.DECIMAL,
-    allowNull: false
-  },
   Variant_Code: {
     type: Sequelize.STRING(10),
     field: 'Variant Code',
     allowNull: false
   },
-  Serial_No: {
-    type: Sequelize.STRING(20),
-    field: 'Serial No_',
-    allowNull: false
-  },
-  Serial_Lot_No_Not_Valid: {
-    type: Sequelize.INTEGER,
-    field: 'Serial_Lot No_ Not Valid',
-    allowNull: false
-  },
-  Lot_No: {
-    type: Sequelize.INTEGER,
-    field: 'Lot No_',
-    allowNull: false
-  },
-  Expiration_Date: {
-    type: Sequelize.DATE,
-    field: 'Expiration Date',
-    allowNull: false
-  },
-  Line_Discount: {
-    type: Sequelize.DECIMAL,
-    field: 'Line Discount',
-    allowNull: false
-  },
-  Replicated: {
-    type: Sequelize.INTEGER,
-    allowNull: false
-  },
-  Customer_Discount: {
-    type: Sequelize.DECIMAL,
-    field: 'Customer Discount',
-    allowNull: false
-  },
-  Infocode_Discount: {
-    type: Sequelize.DECIMAL,
-    field: 'Infocode Discount',
-    allowNull: false
-  },
-  Cust_Invoice_Discount: {
-    type: Sequelize.DECIMAL,
-    field: 'Cust_ Invoice Discount',
-    allowNull: false
-  },
-  Unit_Of_Measure: {
-    type: Sequelize.STRING(10),
-    field: 'Unit of Measure',
-    allowNull: false
-  },
-  UOM_Quantity: {
-    type: Sequelize.DECIMAL,
-    field: 'UOM Quantity',
-    allowNull: false
-  },
-  UOM_Price: {
-    type: Sequelize.DECIMAL,
-    field: 'UOM Price',
-    allowNull: false
-  },
-  Total_Discount: {
-    type: Sequelize.DECIMAL,
-    field: 'Total Discount',
-    allowNull: false
-  },
-  Total_Disc_Perc: {
-    type: Sequelize.DECIMAL,
-    field: 'Total Disc_%',
-    allowNull: false
-  },
-  Tot_Disc_Info_Line_No: {
-    type: Sequelize.INTEGER,
-    field: 'Tot_ Disc Info Line No_',
-    allowNull: false
-  },
-  Periodic_Disc_Type: {
-    type: Sequelize.INTEGER,
-    field: 'Periodic Disc_ Type',
-    allowNull: false
-  },
-  Periodic_Disc_Group: {
-    type: Sequelize.STRING(20),
-    field: 'Periodic Disc_ Group',
-    allowNull: false
-  },
-  Periodic_Discount: {
-    type: Sequelize.DECIMAL,
-    field: 'Periodic Discount',
-    allowNull: false
-  },
-  Deal_Line: {
-    type: Sequelize.INTEGER,
-    field: 'Deal Line',
-    allowNull: false
-  },
-  Discount_Amt_For_Printing: {
-    type: Sequelize.DECIMAL,
-    field: 'Discount Amt_ For Printing',
-    allowNull: false
-  },
-  Replication_Counter: {
-    type: Sequelize.INTEGER,
-    field: 'Replication Counter',
-    allowNull: false
-  },
-  Sales_Type: {
-    type: Sequelize.STRING(20),
-    field: 'Sales Type',
-    allowNull: false
-  },
-  Orig_Trans_Store: {
-    type: Sequelize.STRING(10),
-    field: 'Orig Trans Store',
-    allowNull: false
-  },
-  Orig_Trans_Pos: {
-    type: Sequelize.STRING(10),
-    field: 'Orig Trans Pos',
-    allowNull: false
-  },
-  Orig_Trans_No: {
-    type: Sequelize.INTEGER,
-    field: 'Orig Trans No_',
-    allowNull: false
-  },
-  Orig_Trans_Line_No: {
-    type: Sequelize.INTEGER,
-    field: 'Orig Trans Line No_',
-    allowNull: false
-  },
   Refund_Qty: {
     type: Sequelize.DECIMAL,
     field: 'Refund Qty_',
-    allowNull: false
-  },
-  Refunded_Line_No: {
-    type: Sequelize.INTEGER,
-    field: 'Refunded Line No_',
-    allowNull: false
-  },
-  Refunded_Trans_No: {
-    type: Sequelize.INTEGER,
-    field: 'Refunded Trans_ No_',
-    allowNull: false
-  },
-  Refunded_POS_No: {
-    type: Sequelize.STRING(20),
-    field: 'Refunded POS No_',
-    allowNull: false
-  },
-  Refunded_Store_No: {
-    type: Sequelize.STRING(20),
-    field: 'Refunded Store No_',
     allowNull: false
   }
 }, {
