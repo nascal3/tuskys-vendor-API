ac8f6b4554f9ece689758eaa51965a711120bbe8
nascal3 <nascal3@gmail.com>
2019-02-18 09:50:54 +0300

    edited detailed vendor ledger entry to important columns only

--- models/DetailedVendorLedgEntryModel.js
+++ models/DetailedVendorLedgEntryModel.js
@@ -44,12 +44,7 @@ const detailedVendorLedgEntryModel = sequelize.define('Detailed_Vendor_Ledg_Entr
   },
   Amount_LCY: {
     type: Sequelize.DECIMAL,
-    field: '"Amount (LCY)"',
-    allowNull: false
-  },
-  Inventory_Posting_Group: {
-    type: Sequelize.STRING(10),
-    field: 'Inventory Posting Group',
+    field: "[Amount (LCY)]",
     allowNull: false
   },
   Vendor_No: {
@@ -57,34 +52,14 @@ const detailedVendorLedgEntryModel = sequelize.define('Detailed_Vendor_Ledg_Entr
     field: 'Vendor No_',
     allowNull: false
   },
-  Currency_Code: {
-    type: Sequelize.STRING(10),
-    field: 'Currency Code',
-    allowNull: false
-  },
   User_ID: {
     type: Sequelize.STRING(20),
     field: 'User ID',
     allowNull: false
   },
-  Source_Code: {
-    type: Sequelize.STRING(10),
-    field: 'Source Code',
-    allowNull: false
-  },
   Transaction_No: {
     type: Sequelize.INTEGER,
     field: 'Transaction No_',
-    allowNull: false
-  },
-  Journal_Batch_Name: {
-    type: Sequelize.STRING(10),
-    field: 'Journal Batch Name',
-    allowNull: false
-  },
-  Reason_Code: {
-    type: Sequelize.STRING(10),
-    field: 'Reason Code',
     allowNull: false
   },
   Debit_Amount: {
@@ -99,90 +74,12 @@ const detailedVendorLedgEntryModel = sequelize.define('Detailed_Vendor_Ledg_Entr
   },
   Debit_Amount_LCY: {
     type: Sequelize.INTEGER,
-    field: '"Debit Amount (LCY)"',
+    field: "[Debit Amount (LCY)]",
     allowNull: false
   },
   Credit_Amount_LCY: {
     type: Sequelize.INTEGER,
-    field: '"Credit Amount (LCY)"',
-    allowNull: false
-  },
-  Initial_Entry_Due_Date: {
-    type: Sequelize.DATE,
-    field: 'Initial Entry Due Date',
-    allowNull: false
-  },
-  Initial_Entry_Global_Dim_1: {
-    type: Sequelize.STRING(20),
-    field: 'Initial Entry Global Dim_ 1',
-    allowNull: false
-  },
-  Initial_Entry_Global_Dim_2: {
-    type: Sequelize.STRING(20),
-    field: 'Initial Entry Global Dim_ 2',
-    allowNull: false
-  },
-  Gen_Bus_Posting_Group: {
-    type: Sequelize.STRING(10),
-    field: 'Gen_ Bus_ Posting Group',
-    allowNull: false
-  },
-  Gen_Prod_Posting_Group: {
-    type: Sequelize.STRING(10),
-    field: 'Gen_ Prod_ Posting Group',
-    allowNull: false
-  },
-  Use_Tax: {
-    type: Sequelize.INTEGER,
-    field: 'Use Tax',
-    allowNull: false
-  },
-  VAT_Bus_Posting_Group: {
-    type: Sequelize.STRING(10),
-    field: 'VAT Bus_ Posting Group',
-    allowNull: false
-  },
-  VAT_Prod_Posting_Group: {
-    type: Sequelize.STRING(10),
-    field: 'VAT Prod_ Posting Group',
-    allowNull: false
-  },
-  Initial_Document_Type: {
-    type: Sequelize.INTEGER,
-    field: 'Initial Document Type',
-    allowNull: false
-  },
-  Applied_Vend_Ledger_Entry_No: {
-    type: Sequelize.INTEGER,
-    field: 'Applied Vend_ Ledger Entry No_',
-    allowNull: false
-  },
-  Unapplied: {
-    type: Sequelize.INTEGER,
-    allowNull: false
-  },
-  Unapplied_By_Entry_No: {
-    type: Sequelize.INTEGER,
-    field: 'Unapplied by Entry No_',
-    allowNull: false
-  },
-  Remaining_Pmt_Disc_Possible: {
-    type: Sequelize.DECIMAL,
-    field: 'Remaining Pmt_ Disc_ Possible',
-    allowNull: false
-  },
-  Max_Payment_Tolerance: {
-    type: Sequelize.DECIMAL,
-    field: 'Max_ Payment Tolerance',
-    allowNull: false
-  },
-  Tax_Jurisdiction_Code: {
-    type: Sequelize.STRING(10),
-    field: 'Tax Jurisdiction Code',
-    allowNull: false
-  },
-  WHT: {
-    type: Sequelize.INTEGER,
+    field: "[Credit Amount (LCY)]",
     allowNull: false
   }
 }, {
