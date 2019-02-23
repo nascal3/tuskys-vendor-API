93aa4cea1c055dd86194df07b1e038effa018ecb
nascal3 <nascal3@gmail.com>
2019-02-23 12:28:20 +0300

    modifies query for fetching item and its related transactional data

--- routes/items.js
+++ routes/items.js
@@ -23,39 +23,46 @@ router.get('/:page', async (req, res) => {
   res.status(200).json({'result': items});
 });
 
+// FUNCTION - GET TRANSACTIONS FOR SPECIFIC ITEM NUMBER
+const getTrans = async (itemNum, page) => {
+
+  let limit = 50;   // number of records per page
+  let offset;
+
+  const data = await TransSalesEntryModel.findAndCountAll({
+    where: {Item_No: itemNum}
+  });
+  let pageNum = parseInt(page);      // page number
+  pageNum <= 0 ? pageNum = 1 : pageNum = parseInt(page);
+  let pages = Math.ceil(data.count / limit);
+  offset = limit * (pageNum - 1);
+  console.log('>>>', pages);
+
+  let trans = await TransSalesEntryModel.findAll({
+    attributes: {exclude: ['timestamp']},
+    where: {Item_No: itemNum},
+    limit: limit,
+    offset: offset
+  });
+
+  return { 'transactions': trans, 'pages': pages}
+
+};
+
 // GET AN ITEM WITH THE ITEM NUMBER
 router.get('/item/:itemNum/:page', async (req, res) => {
 
-  let limit = 25;   // number of records per page
-  let offset;
+  let itemNumber = req.params.itemNum;
+  let pageNumber = req.params.page;
 
-  let page = parseInt(req.params.page);      // page number
-  page <= 0 ? page = 1 : page = parseInt(req.params.page);
-  offset = limit * (page - 1);
-
-  const item = await ItemModel.findAll({
-    order: [
-      [{model:TransSalesEntryModel, as: 'Sales'}, 'Transaction_No', 'DESC']
-    ],
-    limit: limit,
-    offset: offset,
-
-    include: [{
-      model:TransSalesEntryModel,
-      as: 'Sales',
-      attributes: {exclude: ['timestamp']}
-    }],
-    where: { No: req.params.itemNum },
+  const item = await ItemModel.findOne({
+    where: { No: itemNumber },
     attributes: {exclude: ['timestamp']}
   });
 
-  //Filter out only all transactions and store in array
-  const sales = [];
-  item.map(trans => {
-    sales.push(trans.Sales);
-  });
+  let sales = await getTrans(itemNumber, pageNumber);
 
-  res.status(200).json({'result': item[0], 'transactions': sales});
+  res.status(200).json({'item': item, 'transactions': sales.transactions, 'pages': sales.pages});
 });
 
 module.exports = router;
