b149002ca37974b07c713698c525a8c056ae6510
nascal3 <nascal3@gmail.com>
2019-02-25 15:01:56 +0300

    added authentication middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,8 +2,8 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 
 const  auth = (req, res, next) => {
-  const token = req.header('x-auth-token');
-  if (!token) return res.status(401).send('Access denied, no token provided!');
+  const token = req.header('Authorization');
+  if (!token) return res.status(401).json({'Error':'Access denied, no token provided!'});
 
   try {
     req.user = jwt.verify(token, config.get('jwtPrivateKey'));
