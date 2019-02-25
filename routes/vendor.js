6385b6c78a7cb5da59d6309f0496c6a172302b7c
nascal3 <nascal3@gmail.com>
2019-02-25 16:43:40 +0300

    added authentication guard to vendor details endpoints

--- routes/vendor.js
+++ routes/vendor.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
 const Vendor = require('../models/VendorModel');
 require('express-async-errors');
 
 // GET ALL VENDORS
-router.get('/list/:page', async (req, res) => {
+router.get('/list/:page', auth, async (req, res) => {
 
   let limit = 50;   // number of records per page
   let offset;
@@ -23,7 +24,7 @@ router.get('/list/:page', async (req, res) => {
 });
 
 // GET ALL VENDOR BY VENDOR NUMBER
-router.get('/:num', async (req, res) => {
+router.get('/:num', auth, async (req, res) => {
 
   const vendor = await Vendor.findOne({
     where: { No: req.params.num },
