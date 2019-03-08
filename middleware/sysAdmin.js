36b841afd21e30d3f5ac2e6b9a9e6fc4c1f3b758
caleb.nasio@tuskys.com <caleb.nasio@tuskys.com>
2019-03-08 14:31:11 +0300

    corrected access admin system level error message

--- middleware/sysAdmin.js
+++ middleware/sysAdmin.js
@@ -1,4 +1,4 @@
 module.exports = function (req, res, next) {
-    if (req.user.level !== 5) return res.status(403).json({'Error':'Access denied!'});
+    if (req.user.level !== 5) return res.status(403).json({'Error':'Access denied, access level only for system admin!'});
     next();
 };
